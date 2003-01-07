f4e8e1d3129da054f8865df40deb4fdbbd5021cb
huceke <ebsi4711@gmail.com>
2003-01-07 14:54:58 +0000

    Added missing File

---
+++ xboxlib.h
@@ -0,0 +1,4 @@
+extern int VerifySignaturex(void *xbe,int debugout);
+extern int GenarateSignaturex(void *xbe);
+extern int read_rsafromflash(char *filename,unsigned int dumpflag);
+extern int load_rsa(unsigned int dumpflag);
